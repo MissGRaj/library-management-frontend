21ac64c5aece3bfe4a695f9e4c093d50c11e602d
athul <athul@athuls-MacBook-Pro.local>
2026-09-09 13:51:37 +0530

    deployment changes

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -43,7 +43,7 @@ function Books() {
             }
             
             const response = await fetch(
-                `http://localhost:8080/books/search?${params.toString()}`,
+                `https://libary-management-backend-production.up.railway.app/books/search?${params.toString()}`,
                 {
                     method: "GET",
                     headers: {
@@ -121,7 +121,7 @@ function Books() {
         }
 
         const token = localStorage.getItem("token");
-        const response = await fetch("http://localhost:8080/books", {
+        const response = await fetch("https://libary-management-backend-production.up.railway.app/books", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -157,7 +157,7 @@ function Books() {
 
         const token = localStorage.getItem("token");
 
-        const response = await fetch(`http://localhost:8080/books/${id}`, {
+        const response = await fetch(`https://libary-management-backend-production.up.railway.app/books/${id}`, {
             method: "DELETE",
             headers: {
                 "Authorization": `Bearer ${token}`
@@ -198,7 +198,7 @@ function Books() {
         const token = localStorage.getItem("token");
 
         const response = await fetch(
-            `http://localhost:8080/books/${editingId}`,
+            `https://libary-management-backend-production.up.railway.app/books/${editingId}`,
             {
                 method: "PUT",
                 headers: {
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
     const handleSubmit = async(e) => {
         e.preventDefault();
 
-        const response = await fetch("http://localhost:8080/auth/login", {
+        const response = await fetch("https://libary-management-backend-production.up.railway.app/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
